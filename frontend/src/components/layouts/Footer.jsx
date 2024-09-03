72700453e8c11da03e8a031601fd47418ab983d9
powerofknowledge2003 <mahimarajesingh@gmail.com>
2024-09-03 20:14:38 +0530

    Initial commit

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+          Food Delivery website - 2024-25. All Rights Reserved By Mahima...
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/layouts/Search.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+const Search = () => {
+  return (
+    <form>
+      <div className="input-group">
+        <input
+          type="text"
+          placeholder="Search your favourite Restaurant..."
+          id="search_field"
+          className="form-control"
+        />
+        <div className="input-group-append">
+          <button id="search_btn" className="btn">
+            <FaSearch className="fa fa-search" />
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Search;
